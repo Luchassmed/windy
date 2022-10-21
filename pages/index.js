3328734aeda622bd017e74eebf02dd33f39914f5
Luchas Schmidt <112946517+Luchassmed@users.noreply.github.com>
2022-10-21 16:34:12 +0200

    refac: target blank

--- pages/index.js
+++ pages/index.js
@@ -64,19 +64,19 @@ export default function Home() {
           <div className="text-5xl flex justify-center gap-16 py-3 dark:text-white">
             <a
               className="transition ease-in-out delay-200 hover:-translate-y-1 hover:scale-110 duration-300"
-              href="https://www.linkedin.com/in/luchas-schmidt/"
+              href="https://www.linkedin.com/in/luchas-schmidt/" target="_blank"
             >
               <AiFillLinkedin />
             </a>
             <a
               className="transition ease-in-out delay-200 hover:-translate-y-1 hover:scale-110 duration-300"
-              href="https://github.com/Luchassmed"
+              href="https://github.com/Luchassmed" target="_blank"
             >
               <AiFillGithub />
             </a>
             <a
               className="transition ease-in-out delay-200 hover:-translate-y-1 hover:scale-110 duration-300"
-              href="mailto: kapot@live.dk"
+              href="mailto: kapot@live.dk" target="_blank"
             >
               <AiFillMail />
             </a>
